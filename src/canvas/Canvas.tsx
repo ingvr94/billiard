16e1d388d5c29c509aeed8bb015d1ec9d7dabf88
ingvr94 <ingvar94@mail.ru>
2024-03-26 01:02:10 +0300

    setCircleArray issue

--- src/canvas/Canvas.tsx
+++ src/canvas/Canvas.tsx
@@ -207,7 +207,7 @@ const Canvas = ({width,height}: CanvasProps) => {
                 const radius=Math.floor(Math.random()*20)+15
                 let x=Math.random()*(Number(width)-radius*2)+radius
                 let y=Math.random()*(Number(height)-radius*2)+radius
-                circleArray.push(new (Circle as any)(x,y,radius) )
+                setCircleArray([...circleArray,new (Circle as any)(x,y,radius)])
             }
         }
 
